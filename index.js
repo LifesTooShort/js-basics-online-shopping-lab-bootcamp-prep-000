a4f21a378e4ced4050306285546202c7d5793a0b
Erin Penner <epenner0522@gmail.com>
2018-08-02 23:36:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,8 +73,14 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // Empty cart and process order
   cart = getCart();
+  let returnString = "";
   
   if (cardNumber.null) {
-    return ""
+    returnString =  "Sorry, we don't have a credit card on file for you.";
   }
+  else {
+    returnString = `Your total cost is \$${total}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+  }
+  return returnString;
 }
